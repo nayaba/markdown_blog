e850543ea1f67dc1d9dad0380d2926c2014d87f4
nayaba <nabila.ayaba@gmail.com>
2021-08-25 18:01:19 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const articleRouter = require('./routes/articles')
+
+console.log('Server is up and running!')
+
+// set ejs
+app.set('view engine', 'ejs')
+
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    }]
+    res.render('articles/index', { articles: articles }) // like props, passing articles to index.ejs
+})
+
+app.listen(5000)
