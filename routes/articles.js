7813df5f85fba2ed26da6481fbacf0949772a3ec
nayaba <nabila.ayaba@gmail.com>
2021-08-25 23:50:37 -0400

    Add index, but there is a save() bug

--- routes/articles.js
+++ routes/articles.js
@@ -1,15 +1,39 @@
 const express = require('express')
+const { db } = require('./../models/article')
 const router = express.Router()
 const Article = require('./../models/article')
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', { article: new Article() })
 })
 
-router.post('/', (req, res) => {
-    const article = new Article({
-        
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article === null) res.redirect('/')
+    res.render('articles/show', { article: article })
+    // const { id } = req.params
+    // try {
+ 
+    //     db.findById(id)
+    // } catch(error) {
+    //     res.send(error)
+    // }
+})
+
+router.post('/', async (req, res) => {
+    let article = new Article({ // important that this is let not const
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
     })
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+        console.log(article)
+    } catch (e) {
+        console.log(e)
+        res.render('/articles/new', { article: article })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 
 const articleRouter = require('./routes/articles')
 
@@ -12,16 +13,15 @@ console.log('Server is up and running!')
 app.set('view engine', 'ejs')
 
 
-app.use('/articles', articleRouter)
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false })) // unhandled promise cannot read property 'title' of undefined if the urlencoder is *after* app.use
 
 app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    }]
+    const articles = Article.find().sort({
+        createdAt: 'desc'
+    })
     res.render('articles/index', { articles: articles }) // like props, passing articles to index.ejs
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
